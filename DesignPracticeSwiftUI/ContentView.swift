2d9b61a3911509a3978bf6c99a5a3831e9beac2e
Naushad Ali Khan <naushadkhank426@gmail.com>
2026-02-03 23:41:17 +0530

    Initial Commit

---
+++ DesignPracticeSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignPracticeSwiftUI
+//
+//  Created by Naushad Ali Khan on 03/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignPracticeSwiftUI/DesignPracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPracticeSwiftUIApp.swift
+//  DesignPracticeSwiftUI
+//
+//  Created by Naushad Ali Khan on 03/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DesignPracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
